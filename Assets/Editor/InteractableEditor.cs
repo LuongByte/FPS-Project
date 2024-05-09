0119aa2671a1130504e3de733481f21a57cd8036
LuongByte <luong.nguyen7125@gmail.com>
2024-05-09 11:21:52 -0700

    Added Events System for basic interactions

---
+++ Assets/Editor/InteractableEditor.cs
@@ -0,0 +1,36 @@
+using UnityEditor;
+
+[CustomEditor(typeof(Interactable), true)]
+
+public class InteractableEditor : Editor
+{
+    
+    public override void OnInspectorGUI()
+    {
+        //Currently selected game object
+        Interactable interactable = (Interactable)target;
+        if(target.GetType() == typeof(EventOnlyInteractable)){
+            interactable.promptMessage = EditorGUILayout.TextField("Prompt Message", interactable.promptMessage);
+            EditorGUILayout.HelpBox("Can only use Unity Events.", MessageType.Info);
+            if(interactable.GetComponent<InteractionEvent>() == null){
+                interactable.useEvents = true;
+                interactable.gameObject.AddComponent<InteractionEvent>(); 
+            }
+        }
+        else{
+            base.OnInspectorGUI();
+            //Checks if object uses events and if it doesn't contain a component then adds one
+            if(interactable.useEvents){
+                if(interactable.GetComponent<InteractionEvent>() == null){
+                interactable.gameObject.AddComponent<InteractionEvent>();
+                }
+            }
+            //If object does not use events but still has the component then remove it
+            else{
+                if(interactable.GetComponent<InteractionEvent>() != null){
+                    DestroyImmediate(interactable.GetComponent<InteractionEvent>());
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 //Base class for interactables
 public abstract class Interactable : MonoBehaviour
 {
+    //Used to Add or remove InteractionEvent objects
+    public bool useEvents;
+    [SerializeField]
     public string promptMessage;
     //Will be called by player
     public void BaseInteract()
     {
+        if(useEvents){
+            GetComponent<InteractionEvent>().onInteract.Invoke();
+        }
         Interact();
     }
 
