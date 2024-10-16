643e0cbabffc9cffd60204e60e4169ae0ec9d864
LuongByte <luong.nguyen7125@gmail.com>
2024-10-15 18:04:29 -0700

    Added Game Management
    Added Main Menu, Gameover Screen, scene manager

---
+++ Assets/Scripts/GameManager/GameOver.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public void End()
+    {
+        gameObject.SetActive(true);
+    } 
+
+
+    public void Restart()
+    {
+
+    }
+
+    public void Quit()
+    {
+        
+    }
+}
